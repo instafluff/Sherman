6062fea1e4a2fca84b8c466c8bb149f0380ed975
Instafluff <35773245+instafluff@users.noreply.github.com>
2018-11-23 14:09:29 -0800

    Added Sherman Weather

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Sherman! - Voice Commands!", username: process.env.TWITCHUSER, oauth: process.env.OAUTH });
+  res.render('index', { title: "Sherman! - Voice Commands!", username: process.env.TWITCHUSER, oauth: process.env.OAUTH, openweatherkey: process.env.OpenWeatherAPI });
 });
 
 function getRandomInt(max) {
